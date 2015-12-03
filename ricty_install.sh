bb650b9834f147ac2a565171d07862f42acb36ab
HShigehara <chilly_0427@hotmail.co.jp>
2015-12-03 23:51:53 +0900

    make ricty-install.sh

---
+++ ricty_install.sh
@@ -0,0 +1,13 @@
+echo "Starting Installation...";
+sudo apt-get install fontforge;
+git clone https://github.com/yascentur/Ricty.git;
+cd Ricty;
+wget http://levien.com/type/myfonts/Inconsolata.otf;
+wget http://iij.dl.sourceforge.jp/mix-mplus-ipa/59022/migu-1m-20130617.zip;
+unzip migu-1m-20130617.zip;
+mv ./migu-1m-20130617/migu*.ttf . && rm -rf migu-1m-20130617 migu-1m-20130617.zip;
+./ricty_generator.sh Inconsolata.otf migu-1m-regular.ttf migu-1m-bold.ttf;
+sudo mkdir -p /usr/share/fonts/truetype/ricty;
+sudo cp -fv Ricty*.ttf /usr/share/fonts/truetype/ricty;
+sudo fc-cache -fv;
+rm -rf Ricty;
